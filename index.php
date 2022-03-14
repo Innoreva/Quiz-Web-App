1b06147e7c93b671ba471e02355faed8c60c892c
Arihant Agnihotri <72561821+ArihantAgnihotri@users.noreply.github.com>
2022-03-14 17:07:33 +0530

    changes to frontend of index, login, register

--- index.php
+++ index.php
@@ -1,69 +1,36 @@
-<?php
-	require('database.php');
-	session_start();
-	if(isset($_SESSION["email"]))
-	{
-		session_destroy();
-	}
-
-	$ref=@$_GET['q'];
-	if(isset($_POST['login']))
-	{
-		$email = $_POST['email'];
-		$pass = $_POST['password'];
-		$email = stripslashes($email);
-		$email = addslashes($email);
-		$pass = stripslashes($pass);
-		$pass = addslashes($pass);
-		$email = mysqli_real_escape_string($con,$email);
-		$pass = mysqli_real_escape_string($con,$pass);
-		$str = "SELECT * FROM user WHERE email='$email' and password='$pass'";
-		$result = mysqli_query($con,$str);
-		if((mysqli_num_rows($result))!=1)
-		{
-			echo "<center><h3><script>alert('Sorry.. Wrong email (or) Password');</script></h3></center>";
-			header("refresh:0;url=login.php");
-		}
-		else
-		{
-			$_SESSION['logged']=$email;
-			$row=mysqli_fetch_array($result);
-			$_SESSION['name']=$row[1];
-			$_SESSION['id']=$row[0];
-			$_SESSION['email']=$row[2];
-			$_SESSION['password']=$row[3];
-			header('location: welcome.php?q=1');
-		}
-	}
-?>
-
 <!DOCTYPE html>
-<html lang="en">
+<html lang="en" dir="ltr">
 
 <head>
-  <meta charset="UTF-8">
-  <title>LogIn</title>
+  <meta charset="utf-8">
+  <title>Brain Quest</title>
 
   <!-- STYLESHEETS -->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-  <link rel="stylesheet" href="CSS/login_style.css">
+  <link rel="stylesheet" href="CSS/front_style.css">
 
-  <!-- GOOGLE FONTS -->
+
+  <!-- ICON -->
+  <link rel="icon" href="icon.ico">
+
+
+  <!-- FONTS -->
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
   <link href="https://fonts.googleapis.com/css2?family=Audiowide&display=swap" rel="stylesheet">
   <link rel="preconnect" href="https://fonts.gstatic.com">
   <link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&display=swap" rel="stylesheet">
 
+
   <!-- SCRIPTS -->
-  <script src="https://kit.fontawesome.com/9a36302444.js" crossorigin="anonymous"></script>
   <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
 
+  <!-- FONT AWESOME JS -->
+  <script src="https://kit.fontawesome.com/7cec356310.js" crossorigin="anonymous"></script>
 
 </head>
 
 <body>
-
   <section id="navigation-bar">
     <nav class="navbar navbar-expand-lg navbar-dark">
       <a class="navbar-brand" href="../front page/index.html">
@@ -77,11 +44,10 @@
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
           <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
             <li class="nav-item">
-              <a class="nav-link" href="index.php">Home</a>
+              <a class="nav-link active" aria-current="page" href="#home">Home</a>
             </li>
             <li class="nav-item">
-              <a class="nav-link active" aria-current="page" href="#">Log in</a>
-
+              <a class="nav-link" href="login.php">Log in</a>
             </li>
             <li class="nav-item">
               <a class="nav-link" href="register.php">Register</a>
@@ -92,23 +58,28 @@
 
   </section>
 
-  <div class="container">
-    <div class="Login">
-      <h1>Log In</h1>
+  <section id="front-page">
+    <div class="top-container">
+      <h1 class="name">BRAIN QUEST</h1>
+      <i class="fad fa-head-side-brain"></i>
+      <h2 class="subhead">QUIZ TIME</h2>
+      <button class="spec">BEGIN</button>
     </div>
-    <div class="box">
-      <i class="fa-solid fa-user"></i>
-      <input type="username" name="useranme" id="username" placeholder="Username">
+  </section>
+
+
+  <section id="footer">
+    <div class="bottom-container">
+      <p class="love">Made with ❤️ by group D</p>
+      <div class="open-source">
+        <p>This website is <a href="https://github.com/Innoreva/Quiz-Web-App"><strong> open source</strong></a>.</p>
+      </div>
     </div>
-    <div class="box">
-      <i class="fa-solid fa-key"></i>
-      <input type="password" name="password" id="password" placeholder="Password">
-    </div>
-    <button class="spec">Enter</button>
-    <div class="register">
-      <a class="new_here" href="..\registration-page\index.html">New Here ?</a>
-    </div>
-  </div>
+
+
+  </section>
+
+
 </body>
 
 </html>
--- register.php
+++ register.php
@@ -1,9 +1,9 @@
 <?php
 	include("database.php");
 	session_start();
-	
+
 	if(isset($_POST['register']))
-	{	
+	{
 		$name = $_POST['name'];
 		$name = stripslashes($name);
 		$name = addslashes($name);
@@ -21,8 +21,8 @@
 		$college = addslashes($college);
 		$str="SELECT email from user WHERE email='$email'";
 		$result=mysqli_query($con,$str);
-		
-		if((mysqli_num_rows($result))>0)	
+
+		if((mysqli_num_rows($result))>0)
 		{
             echo "<center><h3><script>alert('Sorry.. This email is already registered !!');</script></h3></center>";
             header("refresh:0;url=login.php");
@@ -30,49 +30,86 @@
 		else
 		{
             $str="insert into user set name='$name',email='$email',password='$password',college='$college'";
-			if((mysqli_query($con,$str)))	
+			if((mysqli_query($con,$str)))
 			echo "<center><h3><script>alert('Congrats.. You have successfully registered !!');</script></h3></center>";
 			header('location: welcome.php?q=1');
 		}
     }
 ?>
 
+
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <script src="https://kit.fontawesome.com/8f98b1b459.js" crossorigin="anonymous"></script>
-    <title>LogIn</title>
-    <link href="https://fonts.googleapis.com/css2?family=Baloo+Bhai+2:wght@700&family=Bree+Serif&display=swap"
-        rel="stylesheet">
-    <link rel="stylesheet" href="CSS/registration_style.css">
-</head>
+  <meta charset="UTF-8">
+  <title>Registation</title>
 
+  <!-- STYLESHEETS -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+  <link rel="stylesheet" href="CSS/registration_style.css">
+
+  <!-- GOOGLE FONTS -->
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Audiowide&display=swap" rel="stylesheet">
+  <link rel="preconnect" href="https://fonts.gstatic.com">
+  <link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&display=swap" rel="stylesheet">
+
+  <!-- SCRIPTS -->
+  <script src="https://kit.fontawesome.com/9a36302444.js" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
+
+
+</head>
 <body>
+
+  <section id="navigation-bar">
+    <nav class="navbar navbar-expand-lg navbar-dark">
+      <a class="navbar-brand" href="../front page/index.html">
+        <img src="ASSETS/icon.png" width="50" height="50" class="d-inline-block align-top" alt="">
+        Brain Quest
+      </a>
+      <div class="container-fluid">
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+              <a class="nav-link" href="index.php">Home</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="login.php">Log in</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="#">Register</a>
+            </li>
+        </div>
+      </div>
+    </nav>
+
+  </section>
+
     <div class="container">
         <h1>Register Here</h1>
-        <form method="post" action="register.php">
         <div class="box">
             <i class="fa-solid fa-user"></i>
-            <input type="username" name="name" id="username" placeholder="Username">
-        </div method="post" action="register.php">
+            <input type="username" name="useranme" id="username" placeholder="Username">
+        </div>
         <div class="box">
             <i class="fa-solid fa-envelope"></i>
-            <input type="Email ID" name="email" id="Email ID" placeholder="Email ID">
+            <input type="Email ID" name="" id="Email ID" placeholder="Email ID">
         </div>
         <div class="box">
             <i class="fa-solid fa-building-columns"></i>
-            <input type="College" name="college" id="College" placeholder="College">
+            <input type="College" name="" id="College" placeholder="College">
         </div>
         <div class="box">
             <i class="fa-solid fa-key"></i>
             <input type="password" name="password" id="password" placeholder="Password">
         </div>
-        <button class="btn" name="register">Register</button>
-</form>
+        <button class="spec">Register</button>
     </div>
 </body>
 
